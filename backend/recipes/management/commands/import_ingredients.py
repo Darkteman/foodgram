eb4570d43ef37c5c9bf75c2c063ca6df116786a6
Artem Pishchulin <art.pish@ya.ru>
2023-03-25 16:32:01 +0300

    Add command for load ingredients

---
+++ backend/recipes/management/commands/import_ingredients.py
@@ -0,0 +1,24 @@
+import json
+from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    """
+    Загрузка ингредиентов из json файла в базу данных.
+    """
+
+    def handle(self, *args, **options):
+        with open('data/ingredients.json', 'rb') as file:
+            data = json.load(file)
+            for item in data:
+                ingredient, created = Ingredient.objects.get_or_create(
+                    name=item['name'],
+                    measurement_unit=item['measurement_unit']
+                )
+                if created:
+                    print(f'Добавлен: {item["name"]}, '
+                          f'{item["measurement_unit"]}')
+
+        self.stdout.write(self.style.SUCCESS('База данных успешно заполнена!'))
